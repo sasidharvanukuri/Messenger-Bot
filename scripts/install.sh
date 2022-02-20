82951793084d9fa00c109bcda0bc5538710c203e
sasidhar vanukuri <sasidharreddyvanukuri@gmail.com>
2022-02-20 18:02:53 +0530

    scripts added

---
+++ scripts/install.sh
@@ -0,0 +1,24 @@
+
+cat >> /etc/systemd/system/messenger_bot.service <<EOT
+[Unit]
+Description=Messenger-Bot
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+ExecStart=/home/ubuntu/Messenger-Bot/scripts/run.sh
+RemainAfterExit=no
+Restart=on-failure
+RestartSec=5s
+User=root
+Group=www-data
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=messenger_bot
+[Install]
+WantedBy=multi-user.target
+EOT
+systemctl daemon-reload
+systemctl reload nginx
+systemctl enable messenger_bot.service
+systemctl start messenger_bot.service
---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+cd /home/ubuntu/Messenger-Bot
+git stash
+git pull origin master
+npm i --unsafe-perm
+npm start

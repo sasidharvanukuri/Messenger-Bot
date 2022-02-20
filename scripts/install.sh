e4bc4da095151ba846b182e2cf0e44fad318bfc7
sasidhar vanukuri <sasidharreddyvanukuri@gmail.com>
2022-02-20 18:20:15 +0530

    Unit service installation fixed

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
-
 cat >> /etc/systemd/system/messenger_bot.service <<EOT
 [Unit]
-Description=Messenger-Bot
+Description=Messenger Bot
 After=network.target
 StartLimitIntervalSec=0
+
 [Service]
 Type=simple
 ExecStart=/home/ubuntu/Messenger-Bot/scripts/run.sh
@@ -14,10 +14,13 @@ User=root
 Group=www-data
 StandardOutput=syslog
 StandardError=syslog
-SyslogIdentifier=messenger_bot
+SyslogIdentifier=messenger_bot_server
+
+
 [Install]
 WantedBy=multi-user.target
 EOT
+
 systemctl daemon-reload
 systemctl reload nginx
 systemctl enable messenger_bot.service
